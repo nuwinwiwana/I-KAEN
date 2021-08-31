cec9308860c4ed61b6120e019f8bee03f1d72fce
MUHAMMAD HAZIQ HAMIZI <88481674+haziqhamizi23@users.noreply.github.com>
2021-08-31 22:46:04 +0800

    Update Editprofile Page

---
+++ IKAENSYSTEM/app/src/main/java/com/example/i_kaensystem/EditProfileActivity.java
@@ -0,0 +1,17 @@
+package com.example.i_kaensystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+        private EditText UserName, ChangePassword;
+        private Button DoneButton;
+}
+
+
+
+
