0526891e485cd4980cc8120325e08a57736bf75a
DESKTOP-QNIT7LN\User <zaeefharith@gmail.com>
2021-09-10 01:32:19 +0800

    Completed Forgot password page

--- IKAEN/app/src/main/java/com/example/ikaen/ForgotPassword.java
+++ IKAEN/app/src/main/java/com/example/ikaen/ForgotPassword.java
@@ -1,14 +1,68 @@
 package com.example.ikaen;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import android.os.Bundle;
+import static android.content.ContentValues.TAG;
 
 public class ForgotPassword extends AppCompatActivity {
+    private EditText edtEmail;
+    private Button btnResetPassword;
+    private Button btnBack;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        edtEmail = (EditText) findViewById(R.id.email);
+        btnResetPassword = (Button) findViewById(R.id.submitBtn);
+        btnBack = (Button) findViewById(R.id.BackBtn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = edtEmail.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter your email!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "Email sent.");
+                                }
+                            }
+                        });
+
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
+
 }
