2e9d54e1b2df26372470808086cab5d9f643f565
MUHAMMAD HAZIQ HAMIZI <88481674+haziqhamizi23@users.noreply.github.com>
2021-09-16 21:22:48 +0800

    editprofile design(ui)

--- IKAEN/app/src/main/java/com/example/ikaen/ProfilePage.java
+++ IKAEN/app/src/main/java/com/example/ikaen/ProfilePage.java
@@ -2,8 +2,11 @@ package com.example.ikaen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -72,5 +75,5 @@ public class ProfilePage extends AppCompatActivity {
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
-    }
-}
+            }
+        }
