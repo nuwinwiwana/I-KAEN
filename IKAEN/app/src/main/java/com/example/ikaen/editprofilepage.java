77ac01ac57cd8014034fc1b14fb5a3c9b1ced129
MUHAMMAD HAZIQ HAMIZI <88481674+haziqhamizi23@users.noreply.github.com>
2021-09-19 22:51:51 +0800

    Edit profile

--- IKAEN/app/src/main/java/com/example/ikaen/editprofilepage.java
+++ IKAEN/app/src/main/java/com/example/ikaen/editprofilepage.java
@@ -2,13 +2,19 @@ package com.example.ikaen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class editprofilepage extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editprofilepage);
+        
     }
 }
